ff946ed89315324823652f35e416f46f4d9c86e7
Caitlin Pringle <nekotjie@gmail.com>
2014-05-21 09:58:19 +0200

    Updated e2e scenarios
    Added a couple of scenarios that pass, but the functionality isn't 100%
    yet

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -2,46 +2,68 @@
 
 /* http://docs.angularjs.org/guide/dev_guide.e2e-testing */
 
-describe('Login', function() {
-
-  describe('User login', function() {
+describe('User login', function() {
 
     beforeEach(function() {
-      browser.get('#/login');
+        browser.get('#/login');
     });
 
     it('should log the user in successfully', function() {
-
-        var email = element(by.model('user.mail'));
+        var email = element(by.model('user.emailAddress'));
         email.sendKeys('test@gmail.com');
 
-        var password = element(by.model('user.pass'));
+        var password = element(by.model('user.password'));
         password.sendKeys('123');
 
-        
         var ptor = protractor.getInstance();
         ptor.findElement(protractor.By.className('btn-default')).click();
         expect(ptor.getCurrentUrl()).toContain('/#/'); //hmmmmmmm
-        
-    });
-      
-    it('should direct user to the forgot password page', function() {
-        
-    });
-      
-    it('should indicate incorrect password error', function() {
 
     });
-      
+
+    //needs to be updated when forgot password page has been implemented
     it('should direct user to the forgot password page', function() {
-        
+        //ptor.findElement(protractor.By.className('btn-forgot-password')).click();
+        //expect(ptor.getCurrentUrl()).toContain('/#/'); 
     });
-      
-    it('should direct user to the forgot password page', function() {
+
+    it('should indicate incorrect credentials error due to wrong password', function() {
+
+        var email = element(by.model('user.emailAddress'));
+        email.sendKeys('test@gmail.com');
+
+        var password = element(by.model('user.password'));
+        password.sendKeys('456');
+
+        var ptor = protractor.getInstance();
+        //var emailError = ptor.findElement(protractor.By.className('.error'));
+        //expect(emailError.isDisplayed().toBeTruthy());
+        //emailError.getText().toMatch("Incorrect password or login email entered");
 
     });
- 
-      
-  });
+
+    it('should indicate incorrect credentials error due to wrong email', function() {
+        var email = element(by.model('user.emailAddress'));
+        email.sendKeys('test324@gmail.com');
+
+        var password = element(by.model('user.password'));
+        password.sendKeys('123');
+
+        //var ptor = protractor.getInstance();
+        //var emailError = ptor.findElement(protractor.By.className('.alert .error'));
+    });
+
+    it('should redirect user to home page if he or she wishes to not log in', function() {
+
+    });
+
+    it('should redirect to the login page if trying to load protected page while not authenticated', function() {
+        browser.get('/#/home');
+        var loginURL = browser.getCurrentUrl();
+
+        browser.get('/#/');
+        expect(browser.getCurrentUrl()).toEqual(loginURL);
+    });
+
 });
 
