6eda2ed50e9dae8e821cf9e4c8e69175302223a1
Phillip-Juan van der Berg <phillip@zaralab.com>
2014-05-22 09:50:26 +0200

    File locations updated
    Forgot Password  source

--- app/scripts/controllers/forgotPassCtrl.js
+++ app/scripts/controllers/forgotPassCtrl.js
@@ -2,20 +2,19 @@
 ctrl.controller('forgotPassCtrl',['$scope', function($scope){
 		$scope.title = "Forgot Password",
 		
-		var $promise = $http.post('http://localhost:8000/forgotpassword',userLogin);
-				
-				$promise.then(function(msgLogin){
-					$rootScope.errorMsg = msgLogin.data;
-					if(msgLogin.data.length == 0) {
-						
-						var $userPromise = $http.post('http://localhost:8000/login',userLogin);
-						
-						$userPromise.then(function(msg){
-							$rootScope.loggedUser = msg.data;
-							
-							console.log('success login');
-							$state.go("selectBilling");
-						});
-					}
-				});
+		$scope.forgotPassword = function(user){
+			var $promise = $http.post('http://localhost:8000/forgotpassword',userLogin);
+					
+			$promise.then(function(msgLogin){
+				$rootScope.errorMsg = msgLogin.data;
+				if(msgLogin.data.length == 0) {
+					
+					
+					$rootScope.loggedUser = msg.data;
+					
+					console.log('success login');
+					$state.go("selectBilling");
+				}
+			});
+		}
 	}]) 
